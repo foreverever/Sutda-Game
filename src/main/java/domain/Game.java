c4f5dc078bf5b6d97da4e126ce052a0c144c95e9
TaeSu <lollolzkk@gmail.com>
2019-03-04 13:54:25 +0900

    mvc ver_02 (controller단에 로직을 최소화, 현재는 GameController에서 모든 게임 진행이 이루어지고 있음)

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -2,13 +2,12 @@ package domain;
 
 import dto.GameDto;
 
-import java.util.Random;
-
 public class Game {
     private static User user;
     private static User computer;
     private static Dealer dealer;
     private static Turn turn;
+    private int startUser;
 
     public Game(String name) {
         user = new User(name);
@@ -37,22 +36,46 @@ public class Game {
         //카드 회수 추가 필요(card used = false)
     }
 
-    public boolean oneTurn(int currentTurn) {
-        int startUser = 0;
-        if (currentTurn == 1) {
-            Random random = new Random();   //최초 턴 사용자 결정 (유저 or 컴퓨터 중 랜덤)
-            startUser = random.nextInt(2);
-            if (startUser == 0) return turn.userTurn(user, dealer, currentTurn);
-            return turn.computerTurn(computer,dealer,currentTurn);
-        }
-
-        if(startUser == 0 && currentTurn % 2!=0){
-            return turn.userTurn(user, dealer, currentTurn);
-        }
-        return turn.computerTurn(computer,dealer,currentTurn);
-    }
+//    public boolean oneTurn(int currentTurn) {
+//        if (currentTurn == 1) {
+//            return firstTurn(currentTurn);
+//        }
+//        return nextTurn(currentTurn);
+//    }
+//
+//    private boolean firstTurn(int currentTurn) {
+//        Random random = new Random();   //최초 턴 사용자 결정 (유저 or 컴퓨터 중 랜덤)
+//        startUser = random.nextInt(2);
+//        if (startUser == 0) {
+//            return turn.userTurn(user, dealer, currentTurn);
+//        }
+//        return turn.computerTurn(computer, dealer, currentTurn);
+//    }
+//
+//    private boolean nextTurn(int currentTurn) {
+//        if (startUser == 0) {
+//            if (currentTurn % 2 != 0) {
+//                return turn.userTurn(user, dealer, currentTurn);
+//            }
+//            return turn.computerTurn(computer, dealer, currentTurn);
+//        }
+//
+//        if (currentTurn % 2 != 0) {
+//            return turn.computerTurn(computer, dealer, currentTurn);
+//
+//        }
+//        return turn.userTurn(user, dealer, currentTurn);
+//    }
 
     public GameDto _toGameDto() {
         return new GameDto(user, computer, dealer, turn);
+    }
+
+    public boolean userTurn(int currentTurn, int gameType) {
+        return turn.userTurn(user, dealer, currentTurn, gameType);
+    }
+
+    public boolean computerTurn(int currentTurn) {
+        return turn.computerTurn(computer, dealer, currentTurn);
     }
 }
